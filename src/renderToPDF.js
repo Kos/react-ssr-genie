45146d1eae38345f02a6d3fea04343f0c2e55301
Tomasz Wesołowski <kosashi@gmail.com>
2018-01-21 16:43:51 +0100

    Extract renderToPDF.js

---
+++ src/renderToPDF.js
@@ -0,0 +1,32 @@
+const defaultPageSettings = {
+  format: 'a4',
+  margin: {
+    top: '1cm',
+    right: '1cm',
+    bottom: '1cm',
+    left: '1cm',
+  },
+};
+
+module.exports = async function renderToPDF(content, pageSettings, browser) {
+  const page = await browser.newPage();
+  try {
+    await updatePageContent(page, content);
+    const buffer = await page.pdf({
+      ...defaultPageSettings,
+      ...pageSettings,
+    });
+    return buffer;
+  } finally {
+    await page.close();
+  }
+};
+
+async function updatePageContent(page, content) {
+  const bodyHandle = await page.$('body');
+  try {
+    await page.evaluate((body, content) => (body.innerHTML = content), bodyHandle, content);
+  } finally {
+    await bodyHandle.dispose();
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
+const renderToPDF = require('./renderToPDF');
 const puppeteer = require('puppeteer');
 
 module.exports = async function serve({ port = 8012, routes = {} }) {
@@ -12,15 +13,16 @@ module.exports = async function serve({ port = 8012, routes = {} }) {
     const component = routes[route];
     app.get(route, (req, res) => {
       const props = {};
-      const payload = renderToString(component, props);
-      res.send(payload);
+      const content = renderToString(component, props);
+      res.send(content);
     });
 
     app.get(route + '/pdf', async (req, res) => {
       const props = {};
       const pageSettings = {};
       // const filename = 'page.pdf';
-      const buffer = await renderToPDF(component, props, pageSettings, browser);
+      const content = renderToString(component, props);
+      const buffer = await renderToPDF(content, pageSettings, browser);
       res.writeHead(200, {
         'Content-Type': 'application/pdf',
         // 'Content-Disposition': 'attachment; filename=' + filename,
@@ -36,38 +38,4 @@ module.exports = async function serve({ port = 8012, routes = {} }) {
 function renderToString(component, props) {
   const element = React.createElement(component, props);
   return ReactDOMServer.renderToStaticMarkup(element);
-}
-
-const defaultPageSettings = {
-  format: 'a4',
-  margin: {
-    top: '1cm',
-    right: '1cm',
-    bottom: '1cm',
-    left: '1cm',
-  },
-};
-
-async function renderToPDF(component, props, pageSettings, browser) {
-  const page = await browser.newPage();
-  try {
-    const content = renderToString(component, props);
-    await updatePageContent(page, content);
-    const buffer = await page.pdf({
-      ...defaultPageSettings,
-      ...pageSettings,
-    });
-    return buffer;
-  } finally {
-    await page.close();
-  }
-}
-
-async function updatePageContent(page, content) {
-  const bodyHandle = await page.$('body');
-  try {
-    await page.evaluate((body, content) => (body.innerHTML = content), bodyHandle, content);
-  } finally {
-    await bodyHandle.dispose();
-  }
 }
